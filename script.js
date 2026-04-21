431b8a5505ec3270fecc230efd408020916f38e4
527mbeard <527mbeard@frhsd.com>
2026-04-21 11:51:56 -0400

    added fonts, more styling, enter key, background, all js finished

--- script.js
+++ script.js
@@ -22,6 +22,32 @@ document.getElementById('addTaskBtn').addEventListener('click', function () {
 
 })
 
+document.getElementById('taskInput').addEventListener('keydown', (e) => {
+
+    if (e.key === 'Enter') {
+
+        //get value from input field
+        let taskInput = document.getElementById('taskInput').value
+
+        //check if input is empty
+        // if (taskInput.trim() === '') {
+        //     alert('Please enter a task')
+        //     return
+        // }
+
+        if (taskInput) {
+            //add task to array
+            tasks.push(taskInput)
+            //clear input field
+            document.getElementById('taskInput').value = ''
+            //render tasks to the list
+            displayTasks()
+        }
+    }
+
+})
+
+
 function displayTasks() {
     //get the task list element
     let taskList = document.getElementById('taskList')
@@ -32,12 +58,20 @@ function displayTasks() {
         //create list item
         let li = document.createElement('li')
         //add styling
-        li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
+        li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center', 'taskback')
         //set the innerHTML of the task list to the new list items
         li.innerHTML = `${task} <button class="btn btncheck btn-sm" onclick='removeTask(${index})'>✅</button>`
         //append the new list items to the task list element
         taskList.appendChild(li)
     })
-
-
 }
+
+function removeTask(index) {
+    tasks.splice(index, 1)
+    displayTasks()
+}
+
+document.getElementById('clearTasksBtn').addEventListener('click', function () {
+    tasks = []
+    displayTasks()
+})
